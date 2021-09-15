f714ae7556344bfbc6c7c22c897b75c5cf9b19b1
Robert Petersen <robert.petersen808@gmail.com>
2021-09-15 12:28:25 -0500

    Begain adding redux state

---
+++ src/State/reducers/index.js
@@ -0,0 +1,16 @@
+const initialState = {
+  apod: {},
+  likes: [],
+  searchResults: [],
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "" :
+      return state;
+    default :
+      return state;
+  }
+}
+
+export default reducer;
